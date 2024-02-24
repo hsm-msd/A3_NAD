7729f978145f418431c628e0168a81061c60ffac
hsm-msd <hmsadok1302@conestogac.on.ca>
2024-02-24 14:21:44 -0500

    Assignment 3 code

--- LogService.py
+++ LogService.py
@@ -4,6 +4,7 @@ import datetime
 import threading
 from collections import deque
 import time
+import atexit
 
 
 class LoggingService:
@@ -16,7 +17,15 @@ class LoggingService:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             server_socket.bind((self.host, self.port))
             server_socket.listen(5)
+
+            # Log the server start
+            self.log_message("Server started",
+                             (self.host, self.port), "STARTED")
             print(f"Logging service started on {self.host}:{self.port}")
+
+            # Register a function to log the server stop
+            atexit.register(self.log_message, "Server stopped",
+                            (self.host, self.port), "STOPPED")
 
             while True:
                 conn, addr = server_socket.accept()
@@ -34,21 +43,28 @@ class LoggingService:
         request_times = deque(maxlen=10)
 
         with conn:
-            while True:
-                if len(request_times) == 10 and time.time() - request_times[0] < 1:
-                    # If the client has made 10 requests in less than 1 second, disconnect them
-                    self.log_message("Too many requests", addr, "WARNING")
-                    break
+            try:
+                while True:
+                    if len(request_times) == 10 and time.time() - request_times[0] < 1:
+                        # Rate limiting
+                        # If the client has made many requests(~160 - 150) in less than 1 second, disconnect hims
+                        self.log_message("Too many requests", addr, "WARNING")
+                        break
 
-                data = conn.recv(1024)
-                if not data:
-                    break  # Exit the loop if no more data is received
+                    data = conn.recv(1024)
+                    if not data:
+                        break  # Exit the loop if no more data is received
 
-                # Add the current time to the request times
-                request_times.append(time.time())
+                    # Add the current time to the request times
+                    request_times.append(time.time())
 
-                message = data.decode()
-                self.log_message(message, addr)
+                    message = data.decode()
+                    self.log_message(message, addr)
+            except ConnectionResetError:
+                self.log_message("Connection lost with client",
+                                 addr, "LOST_CLIENT")
+            finally:
+                self.log_message("Connection closed", addr, "DISCONNECTED")
 
     def log_message(self, message, addr, level='INFO'):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
